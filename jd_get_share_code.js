373a703b1095ad6e64613bf11472436cefeff676
@ 夜凉 <81776528+linxi915@users.noreply.github.com>
2021-06-07 02:56:09 +0800

    Update jd_get_share_code.js

--- jd_get_share_code.js
+++ jd_get_share_code.js
@@ -5,7 +5,7 @@
 ============Quantumultx===============
 [task_local]
 #获取互助码
-20 13 * * 6 https://jdsharedresourcescdn.azureedge.net/jdresource/jd_get_share_code.js, tag=获取互助码, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+20 13 * * 6 https://raw.githubusercontent.com/linxi915/shuyuan/shuyuan/jd_get_share_code.js, tag=获取互助码, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
  */
 const $ = new Env("获取互助码");
