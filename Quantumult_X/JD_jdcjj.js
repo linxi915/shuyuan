2c4a9ad3c84e60e01b4604196d4800b0a5bcefab
@ 夜凉 <81776528+linxi915@users.noreply.github.com>
2021-06-15 01:37:32 +0800

    Update JD_jdcjj.js

--- Quantumult_X/JD_jdcjj.js
+++ Quantumult_X/JD_jdcjj.js
@@ -1,17 +1,13 @@
 /*
 京东抽奖机
 更新时间：2021-06-01 08:43
-脚本说明：抽奖活动,有新活动可以@我或者提Issues
-脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+脚本说明：抽奖活动,来自搬运
 // quantumultx
 [task_local]
 #京东抽奖机
 11 1 * * * https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_lotteryMachine.js, tag=京东抽奖机, img-url=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/image/jdlottery.png, enabled=true
-// Loon
-[Script]
-cron "11 1 * * *" script-path=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_lotteryMachine.js,tag=京东抽奖机
-// Surge
-京东抽奖机 = type=cron,cronexp=11 1 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/yangtingxiao/QuantumultX/master/scripts/jd/jd_lotteryMachine.js
+
+
  */
 const $ = new Env('京东抽奖机');
 const STRSPLIT = "|";
